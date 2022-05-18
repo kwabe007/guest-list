a280e5a63498ab6da5a24f637d20f50deb01a101
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2022-05-18 14:31:43 +0200

    Add firebase

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,23 @@
 <script>
   import "../app.scss";
   import ""
+  import { onMount } from "svelte";
+  import { initializeApp } from "firebase/app";
+
+  onMount(() => {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyCWjP6GA2ef8YVOkMDekuFWrNqi6kio-wE",
+      authDomain: "ritmo-guest-list.firebaseapp.com",
+      projectId: "ritmo-guest-list",
+      storageBucket: "ritmo-guest-list.appspot.com",
+      messagingSenderId: "682562677876",
+      appId: "1:682562677876:web:a88aa5d38c28afc088f521"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+  })
 </script>
 
-<slot />
+<slot/>
