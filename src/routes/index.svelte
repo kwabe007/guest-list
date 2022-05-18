e6cba3ef36bcc68ebb265231e462e678dcf45cde
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2022-05-18 13:39:03 +0200

    Add initial guest list table with placeholder data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,25 @@
+<script>
+  const SAMPLE_GUEST_DATA = [
+    {name: "Lárus Aemilianus", isChecked: false},
+    {name: "Finees Milana", isChecked: false},
+    {name: "Mandla Atalanta", isChecked: true},
+  ]
+</script>
+
 <h1 class="mb-4">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p class="secondary">Secondary</p>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Mark</th>
+  </tr>
+  {#each SAMPLE_GUEST_DATA as guest}
+    <tr>
+      <td>{guest.name}</td>
+      <td>{guest.isChecked}</td>
+    </tr>
+  {/each}
+
+</table>
+
