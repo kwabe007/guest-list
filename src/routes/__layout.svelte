6fd752ae300fca1cfaf49622eb5681a49493c830
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2022-05-19 10:39:15 +0200

    Remove firebase init from layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,7 @@
-<script>
+<script context="module">
   import "../app.scss";
-  import { onMount } from "svelte";
-  import { initializeApp } from "firebase/app";
 
-  onMount(() => {
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyCWjP6GA2ef8YVOkMDekuFWrNqi6kio-wE",
-      authDomain: "ritmo-guest-list.firebaseapp.com",
-      projectId: "ritmo-guest-list",
-      storageBucket: "ritmo-guest-list.appspot.com",
-      messagingSenderId: "682562677876",
-      appId: "1:682562677876:web:a88aa5d38c28afc088f521"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-  })
+  // export async function load({ page, })
 </script>
 
 <slot/>
