052fc8a8f9e5447a394c505b0f1be5391251fc29
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2022-05-20 09:27:54 +0200

    Add session cookie creation

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,13 +1,26 @@
 import {cert, getApp, getApps, initializeApp} from "firebase-admin/app";
+import {getAuth} from "firebase-admin/auth";
 
 const adminConfig = {
   credential: cert({
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     clientEmail: import.meta.env.VITE_FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: import.meta.env.VITE_FIREBASE_ADMIN_PRIVATE_KEY,
+    privateKey: import.meta.env.VITE_FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
   })
 }
 
 export const getAdminApp = () => {
   return (getApps().length) ? getApp() : initializeApp(adminConfig);
+}
+
+export const verifyIdToken = (token: string) => {
+  const auth = getAuth(getAdminApp());
+  return auth.verifyIdToken(token);
+}
+
+export const createSessionCookie = (token: string, maxAge: number) => {
+  const expiresIn = maxAge * 1000;
+  const auth = getAuth(getAdminApp());
+  const session = auth.createSessionCookie(token, {expiresIn});
+  return `session=${session}; SameSite=Strict; path=/; HttpOnly; Secure; Max-Age=${expiresIn}`;
 }
--- src/routes/auth/session.ts
+++ src/routes/auth/session.ts
@@ -1,14 +1,34 @@
 import type {RequestHandler, RequestHandlerOutput} from "@sveltejs/kit";
-import type {RequestEvent} from "@sveltejs/kit/types/private";
+import {createSessionCookie, verifyIdToken} from "../../lib/firebaseAdmin";
 
-const RESPONSE_INVALID = { status: 401, body: 'Invalid auth header' };
+const RESPONSE_INVALID_HEADER = { status: 401, body: 'Invalid auth header' };
+const RESPONSE_INVALID_TOKEN = { status: 401, body: 'Invalid auth token' };
 
-export const post: RequestHandler = ({ request }) => {
+const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
+
+export const post: RequestHandler = async ({ request }) => {
   const authHeader = request.headers.get('Authorization');
-  if (!authHeader) return RESPONSE_INVALID;
+  if (!authHeader) return RESPONSE_INVALID_HEADER;
 
   const [scheme, token] = authHeader.split(' ');
 
-  if (scheme !== 'Bearer' || !token) return RESPONSE_INVALID;
+  if (scheme !== 'Bearer' || !token) return RESPONSE_INVALID_HEADER;
 
+  try {
+    const decodedIdToken = await verifyIdToken(token);
+    const { sub, email } = decodedIdToken;
+    const userInfo = { id: sub, email };
+    const sessionCookie = createSessionCookie(token, ONE_WEEK_IN_SECONDS);
+
+    return {
+      status: 200,
+      body: userInfo,
+      headers: {
+        'Set-Cookie': sessionCookie,
+      }
+    }
+
+  } catch {
+    return RESPONSE_INVALID_TOKEN;
+  }
 }
